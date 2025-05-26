831d24c8a7e02cf4c1f28424fa68b950a1a699e3
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-26 15:07:23 +0500

    Escape sequence

--- Day2.py
+++ Day2.py
@@ -1,2 +1,27 @@
-# escape sequence 
+# escape sequence
+
+
+# new line chreacter \n used to create a new line   
 print("Hello World \nthis is a new line \nand ofcourse not the last ")
+
+# tab character \t used to create a tab space
+print("Hello World \tthis is a tab space \tand ofcourse not the last ")
+
+# backslash character \\ used to create a backslash
+print("Hello World \\this is a backslash \\and ofcourse not the last ")
+
+# single quote character \' used to create a single quote
+print("Hello World \'this is a single quote \'and ofcourse not the last ")
+
+# double quote character \" used to create a double quote
+print("Hello World \"this is a double quote \"and ofcourse not the last ")
+
+# backspace character \b used to create a backspace
+print("Hello World \bthis is a backspace \band ofcourse not the last ")
+
+# carriage return character \r used to create a carriage return
+print("Hello World \rthis is a carriage return \rand ofcourse not the last ")
+
+# tilde character \~ used to create a tilde
+print( "Hello World" , 6 , 7 , sep="~", end="~\n")
+
