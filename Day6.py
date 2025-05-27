89b3a022dcfb017d618bb3010c796bd8c8789324
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-27 17:54:53 +0500

    input

--- Day6.py
+++ Day6.py
@@ -4,3 +4,9 @@ Name = input("Enter your name: ")
 print(f"Hello, {Name}! Welcome to the program.")
 # This code snippet demonstrates how to use the input function in Python
 # to get user input and print a greeting message.
+
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+
+# The input function returns a string, so we need to convert it to an integer or float for calculations
+print(f"The sum of {x} and {y} is: {int(x) + int(y)}")
