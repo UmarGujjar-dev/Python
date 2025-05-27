93fd0d5b681bbae3d4865e2f6370d2e6ae468535
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-27 17:39:17 +0500

    Typecasting

--- Day5.py
+++ Day5.py
@@ -1,1 +1,13 @@
 # Type casting
+
+# the conversion of data type in to other data type is called type casting
+
+a = int("5")
+b = int("3")
+print(a +  b)
+
+
+# Type casting in print function
+c = "5"
+d = "3"
+print(int(a) +  int(b))
