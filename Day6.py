bfcba3a729966de96411357409403870537004bf
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-27 17:52:55 +0500

    input

---
+++ Day6.py
@@ -0,0 +1,6 @@
+# input function in python
+
+Name = input("Enter your name: ")
+print(f"Hello, {Name}! Welcome to the program.")
+# This code snippet demonstrates how to use the input function in Python
+# to get user input and print a greeting message.
