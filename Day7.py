09cf663b95d01af2f61ea1996c958e810001ef9a
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-29 17:53:42 +0500

    strings

--- Day7.py
+++ Day7.py
@@ -4,3 +4,20 @@ like = 'Python is fun'
 # you can use triple quotes to create a multi-line string
 like2 = """Python is fun
 It is easy to learn"""
+print(like + like2)
+
+# search string Index 
+print(like[0])  # first character
+print(like[-1])  # last character
+# slicing string
+print(like[0:6])  # first 6 characters
+print(like[7:])  # from 7th character to the end
+# string length
+print(len(like))  # length of the string
+# string methods
+print(like.lower())  # convert to lower case
+print(like.upper())  # convert to upper case
+print(like.title())  # convert to title case
+print(like.replace('fun', 'awesome').title())  # replace a substring and convert to title case
+# string concatenation
+print(like + ' and ' + like2)  # concatenate two strings
