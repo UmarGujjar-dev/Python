667f45b4f4e2a0f966df8e2676d7797e7ae9a8b6
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-29 17:39:26 +0500

    strings

---
+++ Day7.py
@@ -0,0 +1,6 @@
+# strings
+# you can use single or double quotes to create a string
+like = 'Python is fun'
+# you can use triple quotes to create a multi-line string
+like2 = """Python is fun
+It is easy to learn"""
