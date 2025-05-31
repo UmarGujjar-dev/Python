8f497be05935427791f751b345c4c6de504f1808
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-31 22:36:04 +0500

    String Methods

--- string-methods.py
+++ string-methods.py
@@ -1,1 +1,81 @@
 # String Methods
+
+"""Strings are imutable  in Python, meaning once created, they cannot be changed.
+However, you can create new strings based on existing ones using various string methods."""
+
+
+
+
+
+
+
+# 1. capitalize()
+string = "Hello, World!"
+print(string.capitalize())  # Output: Hello, world!
+
+
+
+# 2. casefold()
+print(string.casefold())  # Output: hello, world!
+
+
+
+
+# 3. center()
+print(string.center(30, '-'))  # Output: -------Hello, World!-------
+
+
+
+
+# 4. count()
+print(string.count('o'))  # Output: 2 (counts occurrences of 'o')
+
+
+
+
+# 5. encode()   
+print(string.encode('utf-8'))  # Output: b'Hello, World!' (encodes the string to bytes using UTF-8 encoding)
+
+
+
+
+
+# 6. endswith()
+# 7. expandtabs()
+# 8. find()
+# 9. format()
+# 10. format_map()
+# 11. index()
+# 12. isalnum()
+# 13. isalpha()
+# 14. isascii()
+# 15. isdecimal()
+# 16. isdigit()
+# 17. isidentifier()
+# 18. islower()
+# 19. isnumeric()
+# 20. isprintable()
+# 21. isspace()
+# 22. istitle()
+# 23. isupper()
+# 24. join()
+# 25. ljust()
+# 26. lower()
+# 27. lstrip()
+# 28. partition()
+# 29. replace()
+# 30. rfind()
+# 31. rindex()
+# 32. rjust()
+# 33. rpartition()
+# 34. rsplit()
+# 35. rstrip()
+# 36. split()
+# 37. splitlines()  
+# 38. startswith()
+# 39. strip()
+# 40. swapcase()
+# 41. title()
+# 42. translate()
+# 43. upper()
+# 44. zfill()
