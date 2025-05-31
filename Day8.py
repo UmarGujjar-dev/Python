8530e7de24eb5204a5588a816d17cbd46d125589
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-31 21:25:15 +0500

    strings-slicing

---
+++ Day8.py
@@ -0,0 +1,12 @@
+string = "Gujjar"
+dev = string[0:3]  # slicing the first three characters
+print(dev)  # Output: Guj
+# slicing the last three characters
+last_three = string[-3:]  # slicing the last three characters
+print(last_three)  # Output: jar
+# slicing the middle character(s)
+middle = string[3:5]  # slicing the middle characters
+print(middle)  # Output: ja 
+
+allstr = len(string)  # length of the string
+print(allstr)  # Output: 6
