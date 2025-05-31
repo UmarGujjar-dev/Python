d7b7514b0675e6dabb3770aa3860a0fea58499bb
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-31 21:34:24 +0500

    strings-slicing

--- Day8.py
+++ Day8.py
@@ -10,3 +10,13 @@ print(middle)  # Output: ja
 
 allstr = len(string)  # length of the string
 print(allstr)  # Output: 6
+fruit = "banana"
+print(fruit[0:len(fruit)-2])  # Output: ban
+# slicing the first three characters of 'banana'
+print(fruit[:3])  # Output: ban
+# slicing the last three characters of 'banana'
+print(fruit[-3:])  # Output: ana
+# slicing the middle character(s) of 'banana'
+print(fruit[3:5])  # Output: na
+# slicing the first three characters of 'banana' using negative indexing
+print(fruit[-6:-3])  # Output: ban
