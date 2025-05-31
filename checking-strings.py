9c343c929cf5382602a459123f9faa61ef6133da
umerali34119 <umargujjardeveloper@gmail.com>
2025-05-31 22:49:45 +0500

    String Methods practice

---
+++ checking-strings.py
@@ -0,0 +1,17 @@
+a = "gujjar"
+
+UpperG = a.upper() 
+print(a)
+print(UpperG)  # Output: None, since print() returns None
+print(a)
+
+# precticing traling strings
+b = "!!!!!!!Gujjar!!!!!!!!"
+print(b)  # Output: !!!!!!!!Gujjar!!!!!!!!
+print(b.rstrip('!'))  # Output: Gujjar (removes trailing '!')
+
+print(b.lstrip('!'))  # Output: Gujjar (removes leading '!')
+
+print(b.strip('!'))  # Output: Gujjar (removes leading and trailing '!', ',', and space)
+
+print(b.strip('!').replace('Gujjar', 'Umar Gujjar').upper())  
