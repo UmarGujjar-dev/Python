71fa9a9be5667b24a7d6cd69b92afdd65a256659
umerali34119 <umargujjardeveloper@gmail.com>
2025-06-01 12:59:21 +0500

    else if statments

---
+++ Day9.py
@@ -0,0 +1,14 @@
+# Starting if else statements
+
+
+drivingage = int(input("What is your age? "))
+print(f"You are {drivingage} years old.")
+
+if(drivingage>= 18):
+    print("You are old enough to drive.")
+elif(drivingage < 18 and drivingage >= 16):
+    print("You can drive with a learner's permit.")
+elif(drivingage < 0):
+    print("Congratulations on being born!")
+else:
+    print("You are not old enough to drive yet.")
