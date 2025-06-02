5f63aa4332ec8f37aed1ff5150121ddf6b81b1b9
umerali34119 <umargujjardeveloper@gmail.com>
2025-06-03 00:05:54 +0500

    for loop

--- forloop.py
+++ forloop.py
@@ -7,9 +7,22 @@
 
 
 
-colors = ["red", "green", "blue", "yellow", "black"] 
-for color in colors:
-    print(color)
-    for i in color:
-        print(i)
+# colors = ["red", "green", "blue", "yellow", "black"] 
+# for color in colors:
+#     print(color)
+#     for i in colors .range(1, 6):
+#         print(i)
 
+
+
+# for k in range(9, 11):
+#     for j in range(1, 11):
+#         print(f"{k} * {j} = {k * j}")
+#     print("----------")
+
+name = "Umar"
+for b in name:
+    print(b)
+for i in range(1, 25):   
+    print(f"{i} -- {name}")
+    
