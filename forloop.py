3f45376cc5cec8d790ca4383dbfe374b1475ba3d
umerali34119 <umargujjardeveloper@gmail.com>
2025-06-02 23:48:52 +0500

    for loop

--- forloop.py
+++ forloop.py
@@ -1,8 +1,15 @@
 # For loop example
 
-Name = "Umar Gujjar"
-for rep in Name:
-    print(rep.count(Name) , rep)
+# Name = "Umar Gujjar"
+# for rep in Name:
+#     print(rep.count(Name) , rep)
 
 
+
+
+colors = ["red", "green", "blue", "yellow", "black"] 
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
 
