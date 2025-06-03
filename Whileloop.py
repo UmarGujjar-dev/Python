6d52c0bca1ca317eb290b1c7e04ad477ed330052
umerali34119 <umargujjardeveloper@gmail.com>
2025-06-03 17:59:23 +0500

    while loop

---
+++ Whileloop.py
@@ -0,0 +1,17 @@
+# now practicing while loops 
+
+clientvalue1 = int(input("Enter a starting number: "))
+clientvalue = int(input("Enter an ending number: "))
+
+while clientvalue1 <= clientvalue:
+    print(clientvalue1)
+    clientvalue1 += 1
+
+
+
+i = int(input("Enter a number to count down from: "))
+while i <= 10:
+    i = int(input("Enter a number to "))
+    print(i)
+
+print("Countdown complete!")
