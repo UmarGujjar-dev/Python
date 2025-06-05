a64512a7d1c48243a27550675b0f98fb5e530422
umerali34119 <umargujjardeveloper@gmail.com>
2025-06-05 10:42:40 +0500

    while loop

--- Whileloop.py
+++ Whileloop.py
@@ -35,8 +35,34 @@
 
 
 # table print 
-a = int(input("Enter a number to print its multiplication table: "))
-i = 1
-while i <= 10:
-    print(f"{a} * {i} = {a * i}")
-    i += 1
+
+# a = int(input("Enter a number to print its multiplication table: "))
+# i = 1
+# while i <= 10:
+#     print(f"{a} * {i} = {a * i}")
+#     i += 1
+
+# num of list 
+# num = [1, 2, 3, 4, 5, 66, 7, 8, 9, 10]
+
+# i = 0
+# while i < len(num):
+#     print( i , ":", num[i])
+#     i += 1
+
+
+# while i == num[5]:
+# #     print("This is the 6th element in the list:", num[i])
+#     print("This is the 6th element in the list:", num[i])
+#     i += 1
+
+
+# Pyramid Pattern with Odd Numbers
+
+a = "*"
+for i in range(1, 21, 2):  # Start at 1, stop before 21, step by 2 (odd numbers)
+    print((a * i).center(20))
+for i in range(1, 21, 2):  # Start at 1, stop before 21, step by 2 (odd numbers)
+    print((a * i).rjust(20))
+for i in range(1, 21, 2):  # Start at 1, stop before 21, step by 2 (odd numbers)
+    print((a * i))
